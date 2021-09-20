a80c6da7796c09d667e1116855bb547218c86e44
chihoyoon <chiho.yoon@hotmail.com>
2021-09-20 01:36:58 -0600

    setup mysql

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db');
 
 router.get('/', (req, res) => {
     console.log('http://localhost:5000/api/');
+    console.log(db.query('select * from book'))
     res.send({title: 'Booking Now!!'})
 });
 
